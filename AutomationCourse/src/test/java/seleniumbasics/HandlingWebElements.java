96ba0560800edaeb6844f85d862b541db36b4188
Zartech <Zartech@DESKTOP-US8B4S0>
2026-02-14 14:07:46 +0530

    committedonfeb14

--- AutomationCourse/src/test/java/seleniumbasics/HandlingWebElements.java
+++ AutomationCourse/src/test/java/seleniumbasics/HandlingWebElements.java
@@ -1,7 +1,15 @@
 package seleniumbasics;
 
+import java.time.Duration;
+import java.util.NoSuchElementException;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HandlingWebElements extends Base {
 	
@@ -14,6 +22,12 @@ public class HandlingWebElements extends Base {
 		WebElement showmsgButton = driver.findElement(By.xpath("//button[@id='button-one']"));
 		System.out.println(showmsgButton.isDisplayed());
 		System.out.println(showmsgButton.isEnabled());
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));//explicit wait
+		wait.until(ExpectedConditions.elementToBeClickable(showmsgButton));
+		Wait<WebDriver>fluentwait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(5))
+		 .pollingEvery(Duration.ofSeconds(1)).ignoring(NoSuchElementException.class);//fluent wait
+		fluentwait.until(ExpectedConditions.elementToBeClickable(showmsgButton));
 		showmsgButton.click();
 		
 		WebElement textMessage = driver.findElement(By.id("message-one"));
